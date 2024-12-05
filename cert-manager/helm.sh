e1a33bb5e97a9d88874ac66a5c3557d71a0ab703
kallewasnowicz <codeberg@wnsrt.de>
2024-12-05 16:50:33 +0100

    updates

--- cert-manager/helm.sh
+++ cert-manager/helm.sh
@@ -2,7 +2,7 @@
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm upgrade --install cert-manager jetstack/cert-manager \
-  --namespace cert-manager \
+  --namespace default \
   --create-namespace \
   --version v1.13.2 \
   --set installCRDs=true \
