cfd2490765d335874703a31b1aa3d7cc830f4ecb
kallewasnowicz <codeberg@wnsrt.de>
2024-12-18 14:58:49 +0100

    .

--- cert-manager/helm.sh
+++ cert-manager/helm.sh
@@ -2,7 +2,7 @@
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm upgrade --install cert-manager jetstack/cert-manager \
-  --namespace default \
+  --namespace cert-manager \
   --create-namespace \
   --version v1.13.2 \
   --set installCRDs=true \
--- system-upgrade-controller/manifest.sh
+++ system-upgrade-controller/manifest.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-c
-ontroller.yaml
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml
 kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/crd.yaml
