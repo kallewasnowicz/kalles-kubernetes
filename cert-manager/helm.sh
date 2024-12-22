c7d4033e5f5f0110bbcd60f9ad28c2ad5877cf97
kallewasnowicz <codeberg@wnsrt.de>
2024-12-22 13:37:33 +0100

    version update

--- cert-manager/helm.sh
+++ cert-manager/helm.sh
@@ -4,6 +4,6 @@ helm repo update
 helm upgrade --install cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
-  --version v1.13.2 \
-  --set installCRDs=true \
+  --version v1.16.2 \
+  --set crds.enabled=true \
   --set 'extraArgs={--dns01-recursive-nameservers-only,--dns01-recursive-nameservers=8.8.8.8:53\,1.1.1.1:53}'
