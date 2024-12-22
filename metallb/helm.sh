24a3017fc36efd00b35cd7eb5cdff0a135869503
kallewasnowicz <codeberg@wnsrt.de>
2024-12-22 15:20:52 +0100

    updates

--- metallb/helm.sh
+++ metallb/helm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 helm repo add metallb https://metallb.github.io/metallb
 #helm upgrade metallb metallb/metallb
-helm --namespace metallb-system install --create-namespace metallb metallb/metallb
+helm install metallb metallb/metallb --namespace metallb-system --create-namespace
