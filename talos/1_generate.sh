fb37c32b675477913093a6bc9a667e5aca561faf
kallewasnowicz <codeberg@wnsrt.de>
2025-01-12 11:39:04 +0100

    Subfolders

--- talos/1_generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-talosctl gen config --with-secrets secrets.yaml homelab https://10.10.20.101:6443 --force #--config-patch @cilium.patch
-talosctl machineconfig patch controlplane.yaml --patch @master1.patch --output master1.yaml
-talosctl machineconfig patch worker.yaml --patch @worker1.patch --output worker1.yaml
-talosctl machineconfig patch worker.yaml --patch @worker2.patch --output worker2.yaml
--- talos/3_bootstrap.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-talosctl bootstrap --nodes 10.10.20.101 --endpoints 10.10.20.101 --talosconfig=./talosconfig
